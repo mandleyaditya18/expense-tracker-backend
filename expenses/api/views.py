c6277f1fd9f5ab97ee45d0591ee2d4645cf2f720
Aditya Mandley <mandleyaditya18@gmail.com>
2024-09-22 13:08:58 +0530

    delete expense

--- expenses/api/views.py
+++ expenses/api/views.py
@@ -1,15 +1,17 @@
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
 
 from .serializers import ExpenseSerializer
+from expenses.models import Expense
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return self.request.user.expenses.all().order_by("-created_at")
+        return self.request.user.expenses.filter(is_deleted=False).order_by("-created_at")
     
     def get_serializer_context(self):
         return {'request': self.request}
@@ -24,3 +26,9 @@ class ExpenseViewSet(viewsets.ModelViewSet):
     
     def update(self, request, *args, **kwargs):
         return super().update(request, *args, **kwargs)
+    
+    def destroy(self, request, *args, **kwargs):
+        expense = get_object_or_404(Expense, id=kwargs['pk'], user=request.user)
+        expense.is_deleted = True
+        expense.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- expenses/models.py
+++ expenses/models.py
@@ -27,6 +27,7 @@ class Expense(BaseModel):
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     category = models.ManyToManyField(ExpenseCategory, related_name="expenses")
     frequency = models.CharField(max_length=255, choices=EXPENSE_FREQUENCY_CHOICES, default="one_time")
+    is_deleted = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
