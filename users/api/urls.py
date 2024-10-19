b68b7ac6cf9d19af3359739c3e7137c44f7f39ef
Aditya Mandley <mandleyaditya18@gmail.com>
2024-10-19 11:37:54 +0530

    add total_expense_by_month api

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 from rest_framework_simplejwt.views import (
@@ -6,10 +7,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+router.register(r"dashboard", views.DashboardViewSet, basename="dashboard")
+
 urlpatterns = [
     # path('', views.getRoutes),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('', include(router.urls))
 ]
--- users/api/views.py
+++ users/api/views.py
@@ -1,6 +1,11 @@
-from rest_framework import status
+from datetime import datetime
+from django.db.models import Sum
+from expenses.models import Expense
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from .serializers import RegisterSerializer, LoginSerializer, MyTokenObtainPairSerializer
 
 class RegisterView(APIView):
@@ -13,7 +18,7 @@ class RegisterView(APIView):
             refresh = token_serializer.get_token(user)
             access_token = refresh.access_token
             return Response({
-                "refresh": str(refresh), 
+                "refresh": str(refresh),
                 "access": str(access_token)
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -31,3 +36,37 @@ class LoginView(APIView):
                 'access': str(refresh.access_token),
             })
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class DashboardViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=False, methods=['get'])
+    def total_expense_by_month(self, request):
+        month = request.query_params.get("month")
+
+        if not month:
+            return Response(
+                {"error": "Month params is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            date = datetime.strptime(month, "%m-%y")
+            print({date})
+            total_expense = Expense.objects.filter(
+                user=request.user,
+                date__year=date.year,
+                date__month=date.month,
+                is_deleted=False
+            ).aggregate(total=Sum("amount"))["total"] or 0
+
+            return Response({
+                "month": month,
+                "total_expense": total_expense
+            })
+        except:
+            return Response(
+                {"error": "Invalid month format. Use MM-YY."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
